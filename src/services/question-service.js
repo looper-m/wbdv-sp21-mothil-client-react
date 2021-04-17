15b47c9e5fbc92fbc6673466ee9ca3751f52a5b4
looper-m <mothil.vu@gmail.com>
2021-04-16 21:41:26 -0700

    Fix question service URL

--- src/services/question-service.js
+++ src/services/question-service.js
@@ -1,4 +1,4 @@
-const QUESTIONS_URL = `http://localhost:4000/api/quizzes`
+const QUESTIONS_URL = `https://wbdv-sp21-mothil-server-node.herokuapp.com/api/quizzes`
 
 export const findQuestionsForQuiz = (quizId) =>
     fetch(`${QUESTIONS_URL}/${quizId}/questions`)
